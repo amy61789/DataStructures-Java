d37608008fdfb4324cfa303bc7726aeb3d5d3cd6
Amy Funk <amy61789@outlook.com>
2017-10-04 11:11:09 -0700

    Array Average
    User enters 10 numbers (via console), entered into array and averaged.
    -While loop with a try catch for invalid input

---
+++ ArrayAverageFunk01/src/com/program01/arrayaverage/ArrayAverage.java
@@ -0,0 +1,37 @@
+package com.program01.arrayaverage;
+
+/*
+Created by Amy.Funk 10/3/2017
+ */
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ArrayAverage {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int[] numbers = new int[10];
+        int i = 0;
+
+        while (i < numbers.length){
+            try {
+                System.out.println("Please enter a number: ");
+                numbers[i] = sc.nextInt();
+                sum += numbers[i];
+                i++;
+            }
+            catch (InputMismatchException e){
+                System.out.println("Invalid Input.");
+                sc.nextLine();
+            }
+        }
+
+        double average = sum / 10;
+
+        System.out.println("Average value of array elements is: " + average);
+
+    }
+}
